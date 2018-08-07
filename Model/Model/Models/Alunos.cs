715748a13cb1bdf8c70414b9d6adec670cbd5ccc
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-07 16:37:08 -0300

    novo exercício

---
+++ Model/Model/Models/Alunos.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Model.Models
+{
+    public class Alunos
+    {
+        public int Id { get; set; }
+        public string Nome { get; set; }
+        public string Matricula { get; set; }
+        public double Nota1 { get; set; }
+        public double Nota2 { get; set; }
+        public double Nota3 { get; set; }
+        public int frequencia { get; set; }
+    }
+}
