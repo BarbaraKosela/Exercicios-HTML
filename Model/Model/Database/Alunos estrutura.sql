4efe8541180cd3bf396f7d41a568dd1ffbd8f7bf
BarbaraKosela <38699532+BarbaraKosela@users.noreply.github.com>
2018-08-07 16:56:29 -0300

    database

---
+++ Model/Model/Database/Alunos estrutura.sql
@@ -0,0 +1,14 @@
+﻿CREATE TABLE aluno(
+
+id INT IDENTITY (1,1) PRIMARY KEY,
+nome VARCHAR(100) NOT NULL,
+codigo_matricula VARCHAR(100) NOT NULL,
+nota_01 REAL NOT NULL,
+nota_02 REAL NOT NULL,
+nota_03 REAL NOT NULL,
+frequencia TINYINT
+
+);
+
+INSERT INTO aluno(nome,codigo_matricula,nota_01,nota_02,nota_03, frequencia) VALUES 
+('Josézinho', '52', 5, 3, 9, 20);
